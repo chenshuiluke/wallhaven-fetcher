7f47cac359324e2f7326ce6070fba7d8545f30c7
Luke Chen Shui <chenshuiluke@gmail.com>
2016-07-01 12:07:55 -0500

    Fixed bug where multiple random downloads could occur simultaneously

--- src/com/lukechenshui/WallhavenFetcherController.java
+++ src/com/lukechenshui/WallhavenFetcherController.java
@@ -10,6 +10,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.ResourceBundle;
+import java.util.concurrent.ThreadLocalRandom;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -24,6 +25,7 @@ import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.FilenameUtils;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -51,9 +53,15 @@ public class WallhavenFetcherController {
     private ProgressIndicator categorizedProgress; // Value injected by FXMLLoader
 
     @FXML
+    private ProgressIndicator randomProgress;
+
+    private static int numberOfRetriesForRandomlySelectingId = 0;
+
+    @FXML
     void toggleNSFW(MouseEvent event) {
         nsfwEnabled = !nsfwEnabled;
     }
+
     private void updateCategoryProgressIndicator(int val1, int val2){
         Platform.runLater(new Runnable(){
             @Override
@@ -62,6 +70,16 @@ public class WallhavenFetcherController {
             }
         });
     }
+
+    private void updateRandomProgressIndicator(int val1, int val2){
+        Platform.runLater(new Runnable(){
+            @Override
+            public void run(){
+                randomProgress.setProgress((double)val1/val2);
+            }
+        });
+    }
+
     private static boolean isSFW(int id){
         String URL = "https://alpha.wallhaven.cc/wallpaper/" + String.valueOf(id);
         try{
@@ -152,13 +170,17 @@ public class WallhavenFetcherController {
             }
         });
     }
-    private void getPicturesFromIds(ArrayList ids, String path){
+    private void getPicturesFromIds(ArrayList ids, String path, boolean... print){
         for(int counter = 0; counter < ids.size(); counter++){
+
             String downloadingProgressLine = String.valueOf(counter+1) + "/" + String.valueOf(ids.size())
             + " Downloading image :" +  ids.get(counter);
-            updateCategoryProgressIndicator(counter+1, ids.size());
-            System.out.println(downloadingProgressLine);
-            appendToLogs(downloadingProgressLine);
+
+            if(print.length == 0){
+                updateCategoryProgressIndicator(counter+1, ids.size());
+                System.out.println(downloadingProgressLine);
+                appendToLogs(downloadingProgressLine);
+            }
             String[] exts = {".jpg", ".png", ".bmp"};
 
             for(String extension : exts){
@@ -176,9 +198,12 @@ public class WallhavenFetcherController {
             }
 
         }
-        updateCategoryProgressIndicator(1, 0);
+        if(print.length == 0){
+            updateCategoryProgressIndicator(1, 0);
+        }
     }
     private boolean downloadingCategories = false;
+    private boolean downloadingRandomly = false;
     @FXML
     void startCategorizedDownloadOnClick(ActionEvent event) {
         Runnable task = () -> {
@@ -200,10 +225,88 @@ public class WallhavenFetcherController {
         thread.setDaemon(true);
         thread.start();
     }
+    private static boolean checkIfRandomImageAlreadyDownloaded(int id){
+        File random = new File("random");
+        File[] contents = random.listFiles();
+        for(File file : contents){
+            String fileName = file.getName();
+            fileName = FilenameUtils.removeExtension(fileName);
+            if(Integer.valueOf(fileName) == id){
+                numberOfRetriesForRandomlySelectingId = 0;
+                return true;
+            }
+        }
+        numberOfRetriesForRandomlySelectingId++;
+        return false;
+    }
+    private static ArrayList<Integer> getPictureIds(long number){
+        String baseURL = "https://alpha.wallhaven.cc/latest?page=";
+        //System.out.println("Connecting...");
+        ArrayList<Integer> ids = new ArrayList<>();
+        try {
+            for (long counter = 0; counter < number; counter++) {
 
+                Document page = Jsoup.connect(baseURL + String.valueOf(counter + 1)).
+                        timeout(0)
+                        .userAgent("Mozilla/5.0 (X11; Linux x86_64; rv:47.0) Gecko/20100101 Firefox/47.0")
+                        .get();
+                Elements images = page.select("a.preview");
+                for(Element element : images){
+                    String href = element.attr("href");
+                    int pictureId = 0;
+                    Pattern pictureIdRegex = Pattern.compile("\\d+");
+                    Matcher matcher = pictureIdRegex.matcher(href);
+                    if(matcher.find()){
+                        pictureId = Integer.valueOf(matcher.group());
+                        ids.add(pictureId);
+                    }
+                }
+            }
+        }
+        catch(IOException exc){
+            exc.printStackTrace();
+        }
+        return ids;
+    }
     @FXML
     void startRandomDownloadOnClick(ActionEvent event) {
+        Runnable task = () -> {
+            if(numWallpaperTextBox.getText().length() > 0 && !downloadingRandomly) {
+                downloadingRandomly = true;
+                int iterations = Integer.valueOf(numWallpaperTextBox.getText());
+                appendToLogs("Initializing random downloads");
 
+                for(int counter = 0; counter < iterations; counter++){
+                    updateRandomProgressIndicator(counter+1, iterations);
+                    System.out.printf("%d / %d\n ", counter + 1, iterations);
+                    appendToLogsNoNewLine(String.valueOf(counter + 1) + " / " + String.valueOf(iterations) + " ");
+                    ArrayList<Integer> ids = getPictureIds(1);
+                    int max = ids.get(0);
+                    int random;
+                    updateRandomProgressIndicator(-1, 0);
+                    do{
+                        random = ThreadLocalRandom.current().nextInt(1, max + 1);
+                        //random=209773;
+                        //System.out.println(random);
+                        if(numberOfRetriesForRandomlySelectingId > max){
+                            System.out.println("All randomly generated image ids already exist in the random folder as images.");
+                            System.exit(1);
+                        }
+                    }while(checkIfRandomImageAlreadyDownloaded(random) || !isSFW(random));
+                    updateRandomProgressIndicator(counter+1, iterations);
+                    appendToLogs(String.valueOf(random));
+                    ids = new ArrayList<>();
+                    ids.add(random);
+                    getPicturesFromIds(ids, "random", false);
+                }
+                updateRandomProgressIndicator(1, 0);
+                appendToLogs("Done downloading images! Check in the 'random folder");
+                downloadingRandomly = false;
+            }
+        };
+        Thread thread = new Thread(task);
+        thread.setDaemon(true);
+        thread.start();
     }
     private void initializeFolders(){
         String separator = File.separator;
